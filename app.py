6f4a4db5c8f4773dfb4be82245561aac1f085451
Martin T Mathew <martintheodorus4@gmail.com>
2026-02-25 08:08:24 +0530

    Switch routes to deacon_ai Blueprint
    Replace module-level Flask app routes with a deacon_ai Blueprint: add deacon_ai_bp = flask.Blueprint(...), update route decorators from @app.route to @deacon_ai_bp.route, and remove the app = flask.Flask(...) instantiation and the if __name__ == '__main__': app.run(...) block. This modularizes the routes so they can be registered on an application factory.

--- app.py
+++ app.py
@@ -3,17 +3,14 @@ from flask import request, jsonify, render_template
 import os
 import json
 
+deacon_ai_bp = flask.Blueprint('deacon_ai', __name__)
 
-app = flask.Flask(__name__)
-@app.route('/')
+
+@deacon_ai_bp.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/api/data', methods=['POST'])
+@deacon_ai_bp.route('/api/data', methods=['POST'])
 def receive_data():
     data = {}
     return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
